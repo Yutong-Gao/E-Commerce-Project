fc56bf6278dd22726b154540100ea0acc408f6d8
Yutong-Gao <gao.yuto@northeastern.edu>
2023-07-02 14:38:18 -0400

    swith to react dom 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
 import {
@@ -20,30 +20,22 @@ function App() {
       <Router>
         <Navbar />
         <Sidebar />
-        <Switch>
-          {/* <Route exact path="/E-Commerce-Project">
-            <Home />
-          </Route> */}
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/Cart">
-            <Cart />
-          </Route>
-          <Route exact path="/Products">
-            <Products />
-          </Route>
-          <Route exact path="/Products/:id" children={<SingleProduct />} />
-          <PrivateRoute exact path="/Checkout">
-            <Checkout />
-          </PrivateRoute>
-          <Route exact path="*">
-            <Error />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products/:id" element={<SingleProduct />} />
+          <Route path="/products" element={<Products />} />
+          <Route
+            path="/checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
         <Footer />
       </Router>
     </AuthWrapper>
--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -12,14 +12,14 @@ import axios from "axios";
 import { useCartContext } from "../context/cart_context";
 import { useUserContext } from "../context/user_context";
 import { formatPrice } from "../utils/helpers";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const CheckoutForm = () => {
   const { cart, total_amount, shipping_fee, clearCart } = useCartContext();
   const { myUser } = useUserContext();
-  const history = useHistory();
+  const navigate = useNavigate();
   //STRIPE STUFF
 
   const [succeeded, setSucceeded] = useState(false);
@@ -87,8 +87,8 @@ const CheckoutForm = () => {
 
       setTimeout(() => {
         clearCart();
-        history.push("/");
-      }, 10000);
+        navigate.push("/");
+      }, 1000);
     }
   };
   return (
